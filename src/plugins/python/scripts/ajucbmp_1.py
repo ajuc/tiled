e8176bbdf6e43a696e82e8f6656b925d5fb84247
Sebastian Pidek <ajuc00@gmail.com>
2013-09-01 08:51:33 +0200

    Example script (exporting to many bmp files and dsecriptor)

---
+++ src/plugins/python/scripts/ajucbmp_1.py
@@ -0,0 +1,218 @@
+"""
+AjucBmp support for Tiled
+2013, <ajuc00 AT g/\/\ail (om>
+"""
+from tiled import *
+from tiled.qt import *
+import os, sys, struct
+from threading import Thread
+import threading
+
+maps = []
+#threading.stack_size(67108864*2)
+
+def parseName(fn):
+  parts = fn.split(".")
+  return ".".join(parts[:-1]), parts[-1]
+
+def rgbaFromInt(i):
+  # save 5 bits of i to each channel
+  # i can have maximum of 20 bits
+  # (so there are a little more than 1 000 000 tile numbers possible)
+  return [
+    ((i & (31<<15))>>15),
+    ((i & (31<<10))>>10),
+    ((i & (31<<5))>>5),
+    ((i & (31)))
+  ]
+
+class AjucBmp1(Plugin):
+  @classmethod
+  def nameFilter(cls):
+    print ("nameFilter")
+    return "AjucBmp1 (*.bmp)"
+
+  @classmethod
+  def supportsFile(cls, f):
+    print ("supportsFile")
+    return True
+    #open(f).read(4) == 'FORM'
+
+  @classmethod
+  def read(cls, f):
+    print ("read")
+    return False
+
+  @classmethod
+  def bmp_write(cls, d, databytes, filename):
+    mn1 = struct.pack('<B',d['mn1'])
+    mn2 = struct.pack('<B',d['mn2'])
+    filesize = struct.pack('<L',d['filesize'])
+    undef1 = struct.pack('<H',d['undef1'])
+    undef2 = struct.pack('<H',d['undef2'])
+    offset = struct.pack('<L',d['offset'])
+    headerlength = struct.pack('<L',d['headerlength'])
+    width = struct.pack('<L',d['width'])
+    height = struct.pack('<L',d['height'])
+    colorplanes = struct.pack('<H',d['colorplanes'])
+    colordepth = struct.pack('<H',d['colordepth'])
+    compression = struct.pack('<L',d['compression'])
+    imagesize = struct.pack('<L',d['imagesize'])
+    res_hor = struct.pack('<L',d['res_hor'])
+    res_vert = struct.pack('<L',d['res_vert'])
+    palette = struct.pack('<L',d['palette'])
+    importantcolors = struct.pack('<L',d['importantcolors'])
+    #create the outfile
+    outfile = open(filename,'wb')
+    #write the header + the bytes
+    outfile.write(mn1+mn2+filesize+undef1+undef2+offset+headerlength+width+height+\
+                  colorplanes+colordepth+compression+imagesize+res_hor+res_vert+\
+                  palette+importantcolors+databytes)
+    outfile.close()
+
+  @classmethod
+  def getHeader(cls, width, height):
+    return {
+        'mn1':66,
+        'mn2':77,
+        'filesize':0,
+        'undef1':0,
+        'undef2':0,
+        'offset':54,
+        'headerlength':40,
+        'width': width, #200,
+        'height': height, #200,
+        'colorplanes':1,
+        'colordepth':32,
+        'compression':0,
+        'imagesize':0,
+        'res_hor':0,
+        'res_vert':0,
+        'palette':0,
+        'importantcolors':0
+    }
+
+
+  @classmethod
+  def writeDescriptor(cls, m, filesDir, fn):
+    print ("writeDescriptor fn="+fn)
+    return True
+
+  @classmethod
+  def writeTiles(cls, m, fn):
+    print ("writeTiles fn="+fn)
+    return True
+
+  @classmethod
+  def writeSprites(cls, m, fn):
+    print ("writeSprites fn="+fn)
+    return True
+
+  @classmethod
+  def writeMap(cls, m, i, fn):
+    print ("writeMap i="+str(i)+" fn="+fn)
+    padding=None
+    databytes = bytes()
+    l = tileLayerAt(m, i)
+    w=l.width()
+    h=l.height()
+    pixels = [[0, 0, 0, 0] for sth in xrange(w*h)]
+    for y in range(l.height()-1,-1,-1):# (BMPs are L to R from the bottom L row)
+      for x in range(l.width()):
+        if l.cellAt(x, y).tile != None:
+          pixels[x+(h-1-y)*w]=rgbaFromInt(l.cellAt(x, y).tile.id())
+        else:
+          pixels[x+(h-1-y)*w]=rgbaFromInt(0)
+    header_dict = cls.getHeader(w, h)
+    for y in xrange(h):# (BMPs are L to R from the bottom L row)
+      for x in xrange(w):
+        pixel = struct.pack('<BBBB',
+                            pixels[x+(y)*w][0],
+                            pixels[x+(y)*w][1],
+                            pixels[x+(y)*w][2],
+                            pixels[x+(y)*w][3])
+        databytes = databytes + pixel
+      row_mod = (w*header_dict['colordepth']/8) % 4
+      if row_mod == 0:
+          padding = 0
+      else:
+          padding = (4 - row_mod)
+      padbytes = bytes()
+      for i in xrange(padding):
+          column = struct.pack('<B',0)
+          padbytes = padbytes + y
+      databytes = databytes + padbytes
+    print ("PO zrobieniu tablicy. len(databytes)=" + str(len(databytes)))
+    cls.bmp_write(header_dict, databytes, fn)
+    return True
+
+
+  @classmethod
+  def writeObject(cls, obj, f):
+    print ("writeObject")
+    x = [
+      'cell',
+      'height',
+      'property',
+      'shape',
+      'width',
+      'x',
+      'y'
+    ]
+    print ("obj.x="+str(obj.x()))
+    print ("obj.y="+str(obj.y()))
+    print ("obj.width="+str(obj.width()))
+    print ("obj.height="+str(obj.height()))
+    print ("obj.shape="+str(obj.shape()))
+    print ("dir(obj.property)="+str(dir(obj.property)))
+
+    print ("obj.property(\"id\")="+str(obj.property("id")))
+    print ("obj.property(\"spriteNo\")="+str(obj.property("spriteNo")))
+    print ("obj.property(\"somethingElseAltogether\")="+str(obj.property("somethingElseAltogether")))
+
+    print ("dir(obj.cell)="+str(dir(obj.cell())))
+    print ("obj.name="+str(obj.name()))
+    print ("obj.rotation="+str(obj.rotation()))
+    print ("obj.isVisible="+str(obj.isVisible()))
+    return True
+
+  @classmethod
+  def writeObjects(cls, m, i, fn):
+    print ("writeObjects i="+str(i)+" fn="+fn)
+    og = objectGroupAt(m, i)
+    for x in range(og.objectCount()):
+      #print ("dir(og.objectAt("+str(x)+"))="+str(dir(og.objectAt(x))))
+      cls.writeObject(og.objectAt(x), None)
+    return True
+
+
+  @classmethod
+  def write(cls, m, fn):
+    print ("write fn="+fn)
+    name, extension = parseName(fn)
+    allOk = True
+    filesDir = {}
+    for i in xrange(m.layerCount()):
+      if isTileLayerAt(m, i):
+        print ("layer("+str(i)+") is a tile layer")
+        fnTmp = name+"_map_"+str(i)+"_"+extension
+        filesDir[fnTmp] = i
+        allOk = allOk and cls.writeMap(m, i, fnTmp)
+      elif isObjectGroupAt(m, i):
+        print ("layer("+str(i)+") is an object layer")
+        fnTmp = name+"_obj_"+str(i)+"_"+extension
+        filesDir[fnTmp] = i
+        allOk = allOk and cls.writeObjects(m, i, fnTmp)
+
+    fnTmp = name+"_tiles_"+extension
+    filesDir[fnTmp] = 0
+    allOk = allOk and cls.writeTiles(m, fnTmp)
+
+    fnTmp = name+"_sprites_"+extension
+    filesDir[fnTmp] = 0
+    allOk = allOk and cls.writeSprites(m, fnTmp)
+
+    allOk = allOk and cls.writeDescriptor(m, filesDir, fn+"_descriptor_")
+
+    return allOk
+
